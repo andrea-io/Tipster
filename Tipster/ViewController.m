08f3cba018c22f084c4a7f598e01460524553be1
Andrea Gonzalez <andreagonz@fb.com>
2022-06-15 09:31:21 -0700

    Initial Commit

---
+++ Tipster/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by Andrea Gonzalez on 6/15/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
